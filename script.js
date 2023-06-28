46bf0b970c6f7e5cd78fc8ffa52485a40bf43f9b
Rahul Love <100333864+rahul-pvt-18@users.noreply.github.com>
2023-06-28 23:07:15 +0530

    first commit
    Uploading HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelectorAll('.faq-toggle');
+toggleBtn.forEach(toggle => {
+    toggle.addEventListener('click',()=>{
+        toggle.parentNode.classList.toggle('active');
+    })
+});
